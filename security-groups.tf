d492d525196fa517fdb666f58bb3a225c792e056
Collins Orighose <collinsefe@yahoo.com>
2024-10-19 22:03:36 +0100

    add security group

---
+++ security-groups.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "jenkins_security_group" {
+  name        = "jenkins_security_group"
+  description = "Allows Port SSH and HTTP Traffic"
+
+  ingress {
+    description = "Allow SSH Traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow 8080 Traffic"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
