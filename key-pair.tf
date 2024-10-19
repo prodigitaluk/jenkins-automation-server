9576f13f83dd20df0c14516856c4303992b4cf40
Collins Orighose <collinsefe@yahoo.com>
2024-10-19 22:53:02 +0100

    created key pairs

---
+++ key-pair.tf
@@ -0,0 +1,18 @@
+resource "aws_key_pair" "this" {
+  key_name   = "medical-app-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC56JQgYBYnirYqIDoWxcvkuXfAJ895uY0ULLfFcvgBy5zCwm8G9jYq+NyrHloorJHSMgKEbLlTUA5XKaqYD7nlM1FSTyb4W8I3e1+FO0apbRlXe2B3u0yRlNq3x2hFGJlKJwUczEtW1yOwW/WERPccHW3KSkuOhYwZ1r9uhLJF0wLaNQVfY3H6kk5M22/1x7G+MDokq2nDbYWBmaBwUlABcN0bSsqTRwn4OlXH3oZeEkhlik1fAMcScYtZCbPglExlNSjZvMY3kBTfOtq6RaMUHzTCdW0Z8nwsXmxL74si7tSMh4mc+EN9ykjt/e5ClgkPmZG8+GiASOgfBa9rDpYeWnZ3CQ3O7b7YvThUZ2vMbRvbhH0DjgsDKcbE+ONiW4vBmpQHED9QEyAa22s68odaYSvKuhrwy1ZQC5yQnc5UMttmrzSBwMxOeAHz4ibmGUEDQgQcZ+t9qZwnCfFKw2JCL8JgQOw9J8oHfKa3buUnGNLyogK38s2qdq1WfMEVO2c= collinsefe@yahoo.com"
+}
+
+resource "aws_key_pair" "collinsefe" {
+  key_name   = "collinsefe"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC56JQgYBYnirYqIDoWxcvkuXfAJ895uY0ULLfFcvgBy5zCwm8G9jYq+NyrHloorJHSMgKEbLlTUA5XKaqYD7nlM1FSTyb4W8I3e1+FO0apbRlXe2B3u0yRlNq3x2hFGJlKJwUczEtW1yOwW/WERPccHW3KSkuOhYwZ1r9uhLJF0wLaNQVfY3H6kk5M22/1x7G+MDokq2nDbYWBmaBwUlABcN0bSsqTRwn4OlXH3oZeEkhlik1fAMcScYtZCbPglExlNSjZvMY3kBTfOtq6RaMUHzTCdW0Z8nwsXmxL74si7tSMh4mc+EN9ykjt/e5ClgkPmZG8+GiASOgfBa9rDpYeWnZ3CQ3O7b7YvThUZ2vMbRvbhH0DjgsDKcbE+ONiW4vBmpQHED9QEyAa22s68odaYSvKuhrwy1ZQC5yQnc5UMttmrzSBwMxOeAHz4ibmGUEDQgQcZ+t9qZwnCfFKw2JCL8JgQOw9J8oHfKa3buUnGNLyogK38s2qdq1WfMEVO2c= collinsefe@yahoo.com"
+}
+
+
+# % terraform import aws_key_pair.deployer deployer-key
+
+
+import {
+  to = aws_key_pair.collinsefe
+  id = "collinsefe"
+}
